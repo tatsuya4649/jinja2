d291e3c3aed32e58d0d9f2ff95cce8498c51efb1
tatsuya <tatsuya4649developer@gmail.com>
2021-07-03 11:28:37 +0900

    jinja loop

---
+++ ex4/rendering.py
@@ -0,0 +1,11 @@
+from jinja2 import Template,Environment,FileSystemLoader
+import json
+
+env = Environment(loader=FileSystemLoader('./',encoding='utf8'))
+temp = env.get_template('template.html')
+
+with open('parameter.json') as f:
+	param = json.load(f)
+
+render = temp.render(param)
+print(render)
